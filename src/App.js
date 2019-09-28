ce2e179d5d0f7096efcc79c68337239c22a0fc8d
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-28 11:12:41 -0300

    feature: criando um componente com react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+/** Toda vez que for usar a sintaxe do JSX (html no código), deverá
+ * ser usada a biblioteca do react
+ */
+import React from 'react';
+
+function App() {
+  return <h1>Hello ReactJS</h1>
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const soma = (a, b) => a + b;
+import React from 'react';
+/** Integração do React com a árvore de elementos do frontend.
+ * função render -> responsável por renderizar um componente react dentro de um
+ *                  conteúdo do html
+*/
+import { render } from 'react-dom';
 
-alert(soma(1, 4));
+import App from './App';
+
+render(<App />, document.getElementById('app'));
