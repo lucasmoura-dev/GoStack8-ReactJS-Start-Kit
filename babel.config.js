38685f0d7ad24e59d0b6dd6510da52d207492bf2
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-28 10:32:59 -0300

    feature: adicionado as configuracoes do babel

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+/**
+ * "@babel/preset-env": responsável por alterar as funcionalidades do JS que
+ * os navegadores ainda não entem, ex: import/export, arrow functions, classes etc.
+ * "@babel/preset-react": transformar as coisas que o navegador não entem do 
+ * React, como: JSX (html/css no javascript), e outras funcionalidades.
+ */
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+  ],
+};
